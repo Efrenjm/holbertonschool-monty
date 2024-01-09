2694c8a23749f9bf4206a722ea55f4b05b60803e
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-01-08 18:54:02 -0800

    implements inst error

--- main.c
+++ main.c
@@ -70,12 +70,10 @@ instruction_t get_op(int index)
  * @linum: the current monty line number
  * Return: 0 if success, -1 if atoi fails (not pushing a number)
  */
-int exec_push(char *str, stack_t **head, int linum)
+void exec_push(char *str, stack_t **head, int linum)
 {
 	int x = 0, digs = 0;
 	char num[20] = {'\0'};
-
-	(void)linum;
 
 	if (strncmp(str, "push", 4) == 0)
 	{
@@ -83,60 +81,67 @@ int exec_push(char *str, stack_t **head, int linum)
 			digs++;
 		strncpy(num, str + 5, digs);
 		x = atoi(num);
-		if (x != -1)
+		if (x != -1 && digs != 0)
 		{
-			add_dnodeint(head, x);
+			if (staq(0, 0))
+				add_dnodeint(head, x);
+			else
+				add_dnodeint_end(head, x);
 		}
 		else
 		{
-			return (-1);
+			push_error(linum);
+			last_status(-1);
+			return;
 		}
 	}
-	return (0);
 }
 
 /**
  * exec_comm - do a command for an opcode
- * @str: the string at a checkpoint
+ * @s: the string at a checkpoint
  * @head: the head of the stack
  * @linum: the monty line number
  * Return: 1 if success, 0 if nop or comment, -1 if error
  */
-int exec_comm(char *str, stack_t **head, int linum)
+void exec_comm(char *s, stack_t **head, int linum)
 {
-	int i = 0;
+	int i = 0, l = 0;
+	char inst[1024] = {'\0'};
 
 	for (i = 0; i <= 5; i++)
 	{
-		if (strncmp(get_op(i).opcode, str, 3) == 0)
+		if (strncmp(get_op(i).opcode, s, 3) == 0)
 		{
 			get_op(i).f(head, linum);
-			return (1);
+			return;
 		}
 	}
 	for (i = 6; i <= 11; i++)
 	{
-		if (strncmp(get_op(i).opcode, str, 4) == 0)
+		if (strncmp(get_op(i).opcode, s, 4) == 0)
 		{
 			get_op(i).f(head, linum);
-			return (1);
+			return;
 		}
 	}
 	for (i = 12; i <= 14; i++)
 	{
-		if (strncmp(get_op(i).opcode, str, 5) == 0)
+		if (strncmp(get_op(i).opcode, s, 5) == 0)
 		{
 			get_op(i).f(head, linum);
-			return (1);
+			return;
 		}
 	}
-	if ((strncmp(str, "nop", 3) == 0) || (str[0] == '#'))
-		return (0);
-	if (strncmp(str, "push", 4) == 0)
-		return (0);
-	if ((str[0] == '\n') || (!(str[0])))
-		return (0);
-	return (-1);
+	if ((strncmp(s, "nop", 3) == 0) || (s[0] == '#'))
+		return;
+	if ((strncmp(s, "push", 4) == 0) || (s[0] == '\n') || (!(s[0])))
+		return;
+	while ((s[l] != '\n') && (s[l] != ' ') && (s[l] != 9) && (s[l]))
+		l++;
+	strncpy(inst, s, l);
+	inst_error(inst, linum);
+	last_status(-1);
 }
 
 /**
@@ -154,6 +159,7 @@ int main(int argc, char **argv)
 	stack_t *stack = NULL;
 
 	fd = initialize_fd(argc, argv[1]);
+	staq(1, 0);
 
 	while (((red = getline(&line, &length, fd)) != -1) && stat != -1)
 	{
