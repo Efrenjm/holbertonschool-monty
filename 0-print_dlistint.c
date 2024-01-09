bd6b62e45b04445dbbf2655741ce1803fa29f6b6
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-01-08 20:16:26 -0800

    correction en print list

--- 0-print_dlistint.c
+++ 0-print_dlistint.c
@@ -1,16 +1,18 @@
 #include "monty.h"
-#include <stdio.h>
 /**
- * print_dlistint - prints all the elements of a dlistint_t list
- * @h: doubly linked list node
+ * print_dlistint - function that prints all the elements of a dlistint_t list
+ * @h: doubly linked list
  * Return: the number of nodes
  */
-
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t size = 0;
+	int i = 0;
 
-	for (; h; h = h->next, size++)
+	for (i = 0; h != NULL; i++)
+	{
 		printf("%d\n", h->n);
-	return (size);
+		h = h->next;
+	}
+	return (i);
 }
+
