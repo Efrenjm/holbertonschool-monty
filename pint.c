d6f67c57393abf042fa4a83a5927028f8466e202
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-01-08 20:06:35 -0800

    pint added

--- pint.c
+++ pint.c
@@ -1,5 +1,4 @@
 #include "monty.h"
-
 /**
  * pint - prints the top of the stack
  * @stack: pointer to the stack
@@ -8,7 +7,13 @@
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
-	(void)line_number;
+	if (!stack || !(*stack))
+	{
+		pint_error(line_number);
+		last_status(-1);
+		return;
+	}
+
 
 	printf("%d\n", (*stack)->n);
 }
